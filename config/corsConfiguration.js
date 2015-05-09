c44ea23eb2f7cad7fbf772ea966312ce2de66c09
Stilian Kanchev <pechi169@gmail.com>
2015-05-10 02:28:14 +0300

    Cors update

--- config/corsConfiguration.js
+++ config/corsConfiguration.js
@@ -1,7 +1,7 @@
 module.exports = function (config, app) {
     app.use(function (req, res, next) {
-        res.header("Access-Control-Allow-Origin", "*");
-        res.header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+        res.header("Access-Control-Allow-Origin", req.headers.origin || "*");
+        res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,HEAD,DELETE,OPTIONS');
         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
         next();
     });
